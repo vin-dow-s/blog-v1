4a590787a3af03cd117971bd2b6b155f9eea963b
Vincent S <esyfer@free.fr>
2024-11-01 17:32:49 +0100

    pnpm update +react-quilljs -react-quill

--- src/components/PostForm.tsx
+++ src/components/PostForm.tsx
@@ -157,145 +157,144 @@ export const PostForm = ({
         })?.length || 0
 
     return (
-            <Form {...form}>
-                <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-8 px-2"
-                >
-                    <FormField
-                        control={form.control}
-                        name="title"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Title</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        placeholder="Public title of the post."
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormDescription>
-                                    <span
-                                        className={
-                                            titleLength >= 50 &&
-                                            titleLength <= 60
-                                                ? 'text-green-600'
-                                                : 'text-red-600'
-                                        }
-                                    >
-                                        {titleLength} characters.
-                                    </span>{' '}
-                                    Optimised length for SEO: 50-60 characters.
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="categoryId"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Category</FormLabel>
-                                <FormControl>
-                                    <Select
-                                        onValueChange={(value) => {
-                                            field.onChange(Number(value))
-                                        }}
-                                        value={String(field.value)}
-                                    >
-                                        <SelectTrigger className="w-[180px]">
-                                            <SelectValue placeholder="Select Category" />
-                                        </SelectTrigger>
-                                        <SelectContent>
-                                            {categories?.map((category) => (
-                                                <SelectItem
-                                                    key={category.id}
-                                                    value={String(category.id)}
-                                                >
-                                                    {category.name}
-                                                </SelectItem>
-                                            ))}
-                                        </SelectContent>
-                                    </Select>
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="description"
-                        render={({ field }) => (
-                            <FormItem>
-                                <FormLabel>Description</FormLabel>
-                                <FormControl>
-                                    <Input
-                                        placeholder="Ex: Just open this it's juicy"
-                                        {...field}
-                                    />
-                                </FormControl>
-                                <FormDescription>
-                                    Short description of the post for the card
-                                    overview.
-                                </FormDescription>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="content"
-                        render={() => (
-                            <FormItem>
-                                <FormLabel>Content</FormLabel>
-                                <FormControl>
-                                    <ReactQuill
-                                        theme="snow"
-                                        value={form.watch('content')}
-                                        onChange={(value) =>
-                                            form.setValue('content', value)
-                                        }
-                                        modules={quillModules}
-                                        formats={quillFormats}
-                                        className="rounded-md bg-transparent"
-                                    />
-                                </FormControl>
-                                <FormMessage />
-                            </FormItem>
-                        )}
-                    />
-                    <FormField
-                        control={form.control}
-                        name="isPublished"
-                        render={({ field }) => (
-                            <RadioGroup
-                                value={field.value ? 'published' : 'draft'}
-                                onValueChange={(value) =>
-                                    field.onChange(value === 'published')
-                                }
-                            >
-                                <div className="flex items-center space-x-2">
-                                    <RadioGroupItem value="draft" id="draft" />
-                                    <Label htmlFor="draft">Draft</Label>
-                                </div>
-                                <div className="flex items-center space-x-2">
-                                    <RadioGroupItem
-                                        value="published"
-                                        id="published"
-                                    />
-                                    <Label htmlFor="published">Published</Label>
-                                </div>
-                            </RadioGroup>
-                        )}
-                    />
-                    <Button type="submit">
-                        {isEditing ? 'Update Post' : 'Create Post'}
-                    </Button>
-                    <Button variant="secondary" type="reset">
-                        Reset
-                    </Button>
-                </form>
-            </Form>
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(onSubmit)}
+                className="space-y-8 px-2"
+            >
+                <FormField
+                    control={form.control}
+                    name="title"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Title</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Public title of the post."
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                <span
+                                    className={
+                                        titleLength >= 50 && titleLength <= 60
+                                            ? 'text-green-600'
+                                            : 'text-red-600'
+                                    }
+                                >
+                                    {titleLength} characters.
+                                </span>{' '}
+                                Optimised length for SEO: 50-60 characters.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="categoryId"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Category</FormLabel>
+                            <FormControl>
+                                <Select
+                                    onValueChange={(value) => {
+                                        field.onChange(Number(value))
+                                    }}
+                                    value={String(field.value)}
+                                >
+                                    <SelectTrigger className="w-[180px]">
+                                        <SelectValue placeholder="Select Category" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        {categories?.map((category) => (
+                                            <SelectItem
+                                                key={category.id}
+                                                value={String(category.id)}
+                                            >
+                                                {category.name}
+                                            </SelectItem>
+                                        ))}
+                                    </SelectContent>
+                                </Select>
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="description"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Description</FormLabel>
+                            <FormControl>
+                                <Input
+                                    placeholder="Ex: Just open this it's juicy"
+                                    {...field}
+                                />
+                            </FormControl>
+                            <FormDescription>
+                                Short description of the post for the card
+                                overview.
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="content"
+                    render={() => (
+                        <FormItem>
+                            <FormLabel>Content</FormLabel>
+                            <FormControl>
+                                <ReactQuill
+                                    theme="snow"
+                                    value={form.watch('content')}
+                                    onChange={(value) =>
+                                        form.setValue('content', value)
+                                    }
+                                    modules={quillModules}
+                                    formats={quillFormats}
+                                    className="rounded-md bg-transparent"
+                                />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}
+                />
+                <FormField
+                    control={form.control}
+                    name="isPublished"
+                    render={({ field }) => (
+                        <RadioGroup
+                            value={field.value ? 'published' : 'draft'}
+                            onValueChange={(value) =>
+                                field.onChange(value === 'published')
+                            }
+                        >
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem value="draft" id="draft" />
+                                <Label htmlFor="draft">Draft</Label>
+                            </div>
+                            <div className="flex items-center space-x-2">
+                                <RadioGroupItem
+                                    value="published"
+                                    id="published"
+                                />
+                                <Label htmlFor="published">Published</Label>
+                            </div>
+                        </RadioGroup>
+                    )}
+                />
+                <Button type="submit">
+                    {isEditing ? 'Update Post' : 'Create Post'}
+                </Button>
+                <Button variant="secondary" type="reset">
+                    Reset
+                </Button>
+            </form>
+        </Form>
     )
 }
